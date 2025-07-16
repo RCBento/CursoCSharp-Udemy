53d576669c7a39a2ea98873b3965502e09a3065f
RCBento <andrexedu@gmail.com>
2025-07-16 13:59:57 -0300

    Incrementando While, DoWhile, For, ForEach, Break, Continue

--- obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3598fc05403210f353d5e64594b33c6879a5709")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
